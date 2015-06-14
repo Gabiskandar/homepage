be3693e4c88935e5a98f1ed41be7c70ed9ab625a
Phil J. Laszkowicz <phil@fillip.pro>
2015-06-14 18:21:07 +0100

    Made AJAX call async.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 'use strict'
 
-window.onload = loadPage;
-
 var _content = {
   title: "Untitled",
   masthead: {
@@ -15,28 +13,33 @@ var _content = {
   }
 };
 
+window.onload = loadPage;
+
 function loadPage() {
   loadContent();
-
-  var path = '/html/body/header/h1';
-  var headTitle = document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;  
-
-  headTitle.textContent = _content.title;
-  document.title = _content.title;
 }
 
 function loadContent() { 
   var xhttp = new XMLHttpRequest();
 
   xhttp.overrideMimeType('text/xml');
-
-  xhttp.open("GET", "./data/content.xml", false);
+  xhttp.onload = dataLoaded;
+  xhttp.open("GET", "./data/content.xml", true);
   xhttp.send(null);
-  var xmlDoc = xhttp.responseXML;
+}
+
+function dataLoaded() {
+  var xmlDoc = this.responseXML;
 
   var nsResolver = xmlDoc.createNSResolver( xmlDoc.ownerDocument == null ? xmlDoc.documentElement : xmlDoc.ownerDocument.documentElement);
 
   var title = xmlDoc.evaluate('/content/title', xmlDoc, nsResolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;  
 
   _content.title = title.textContent;
+
+  var path = '/html/body/header/h1';
+  var headTitle = document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;  
+
+  headTitle.textContent = _content.title;
+  document.title = _content.title;
 }
